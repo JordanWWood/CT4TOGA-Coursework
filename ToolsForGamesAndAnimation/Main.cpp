6c1a9e02da355a000725944fdea45402bfca1148
Jordan Wood <up826858@myport.ac.uk>
2016-12-06 17:07:54 +0000

    + Texture loading

--- ToolsForGamesAndAnimation/Main.cpp
+++ ToolsForGamesAndAnimation/Main.cpp
@@ -1,25 +1,26 @@
 #include "window.h"
 #include "shader.h"
 #include "mesh.h"
+#include "texture.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLine, int iCmdShow);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLine, int iCmdShow) {
 	Window window(800, 600, 50, 50, "Artifact", hInstance, iCmdShow);
 
-	Vertex vertices[] = { Vertex(glm::vec3(-0.5, -0.5, 0)),
-						  Vertex(glm::vec3(0, 0.5, 0)),
-						  Vertex(glm::vec3(0.5, -0.5, 0)) };
+	Vertex vertices[] = { Vertex(glm::vec3(-0.5, -0.5, 0), glm::vec2(0.0, 0.0)),
+						  Vertex(glm::vec3(0, 0.5, 0), glm::vec2(0.5, 1.0)),
+						  Vertex(glm::vec3(0.5, -0.5, 0), glm::vec2(1.0, 0.0)) };
 
 	Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));
 	Shader shader("./res/basicShader");
+	Texture texture("./res/bricks.jpg");
 
 	while (!window.isClosed()) {
 		window.Clear(0, 0, 0.5, 0);
 
-		
-
 		shader.Bind();
+		texture.Bind(0);
 		mesh.Draw();
 
 		window.Update();
--- ToolsForGamesAndAnimation/mesh.h
+++ ToolsForGamesAndAnimation/mesh.h
@@ -5,10 +5,17 @@
 
 class Vertex {
 	public:
-		Vertex(const glm::vec3& pos) { this->pos = pos; }
+		Vertex(const glm::vec3& pos, const glm::vec2 texCoord) {
+			this->pos = pos;
+			this->texCoord = texCoord;
+		}
+		
+		inline glm::vec3* GetPos() { return &pos; }
+		inline glm::vec2* GetTexCoord() { return &texCoord; }
 	protected:
 	private:
 		glm::vec3 pos;
+		glm::vec2 texCoord;
 };
 
 class Mesh
@@ -23,6 +30,7 @@ class Mesh
 	private:
 		enum {
 			POSITION_VB,
+			TEXCOORD_VB,
 
 			NUM_BUFFERS
 		};
